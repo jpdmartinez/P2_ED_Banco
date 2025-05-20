f3ba642228f8735c2742ff69c7dbc6909c8b8bac
JoseH-s <josehribeirodossantos@gmail.com>
2025-05-20 17:41:11 -0300

    Add files via upload

---
+++ historico.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+
+class Node: 
+    def __init__(self, data: tuple):
+        self.data = data  
+        self.prev = None
+        self.next = None
+
+class LinkedList: 
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def push(self, tipo: str, valor: float):
+        data_hora = datetime.now().strftime("%d/%m/%Y %H:%M")
+        tupla = (tipo, valor, data_hora)
+        novo_no = Node(tupla)  
+        
+        if self.head is None:
+            self.head = self.tail = novo_no
+        else:
+            self.tail.next = novo_no
+            novo_no.prev = self.tail
+            self.tail = novo_no
+
+    def listar(self):
+        atual = self.head
+        print("Histórico de transações:")
+        while atual:
+            tipo, valor, data = atual.data
+            print(f"{data} - {tipo}: R$ {valor:.2f}")
+            atual = atual.next
+    
+def mostrar_historico(operacao):
+    operacao.transacoes.listar()
